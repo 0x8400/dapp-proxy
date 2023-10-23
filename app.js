611f42316b012477296cb4ebf40b92abe5a117b9
Vonki <nswanqi@gmail.com>
2023-10-23 23:08:19 +0800

    init

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+  });
+  app.use((req, res, next) => {
+    const subdomain = req.subdomains[0]; // 获取子域名
+    const targetUrl = config[subdomain]; // 根据子域名获取目标URL
+    if (targetUrl) {
+      createProxyMiddleware({ 
+        target: targetUrl, 
+        changeOrigin: true,
+        onProxyRes: (proxyRes, req, res) => {
+            const end = res.end;
+            const write = res.write;
+            let buffer = Buffer.from('');
+        
+            res.write = (data) => {
+              buffer = Buffer.concat([buffer, data]);
+            };
+        
+            res.end = () => {
+              const html = buffer.toString();
+              const script = '<script>window.ethereum = window.parent.ethereum;</script>';
+              const modifiedHtml = injectScript(html, script);
+              res.setHeader('content-length', modifiedHtml.length);
+              write.call(res, modifiedHtml);
+              end.call(res);
+            };
+          },
+      })(req, res, next);
+    } else {
+      res.status(404).send('Not found');
+    }
+  });
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+const injectScript = (html, script) => {
+    const closingBodyTag = '</body>';
+    if (html.lastIndexOf(closingBodyTag) === -1) {
+      return html;
+    }
+    return html.slice(0, html.lastIndexOf(closingBodyTag)) + script + closingBodyTag;
+  };
